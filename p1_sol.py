1d1fe57f9aaa6110111d19efc8ce74a72de4f39c
leahschneidereit <118493180+leahschneidereit@users.noreply.github.com>
2023-02-13 23:51:18 -0500

    Add files via upload

--- p1_sol.py
+++ p1_sol.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def rot_roll(psi):
 	'''
+        This function returns a matrix for a roll rotation (about the x-axis)
 	'''
 	
 	rot = np.array([[1.0, 0.0, 0.0], [0.0, math.cos(psi), -math.sin(psi)],[0.0, math.sin(psi), math.cos(psi)]])
@@ -11,6 +12,7 @@ def rot_roll(psi):
 	
 def rot_pitch(theta):
 	'''
+        This function returns a matrix for a pitch rotation (about the y axis)
 	'''
 	
 	rot = np.array([[math.cos(theta), 0.0, math.sin(theta)],[0.0,1.0,0.0],[-math.sin(theta), 0.0, math.cos(theta)]])
@@ -19,6 +21,7 @@ def rot_pitch(theta):
 
 def rot_yaw(phi):
 	'''
+        This function returns a matrix for a yaw rotation (about the z-axis)
 	'''
 	
 	rot = np.array([[math.cos(phi), -math.cos(phi), 0.0], [math.sin(phi), math.cos(phi), 0.0],[0.0, 0.0, 1.0]])
@@ -26,5 +29,9 @@ def rot_yaw(phi):
 	return rot
 
 def vec(x, y, z):
+        '''
+        This function initializes the vector orientation as an array of its
+        x, y, and z positioning
+        '''
 	vec = np.array([[x,y,z]]).T
 	return vec

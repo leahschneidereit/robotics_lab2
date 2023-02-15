8f338949480dc725189abb405a81e14472b666c7
leahschneidereit <118493180+leahschneidereit@users.noreply.github.com>
2023-02-14 19:58:41 -0500

    Add files via upload

--- p1_sol.py
+++ p1_sol.py
@@ -1,18 +1,26 @@
+# import necessary modules
 import rbm
 import math
 import numpy as np
 
 if __name__ == "__main__":
+        #initialize angles to pi/2 radians (from problem statement)
 	theta = math.pi / 2
 	phi = math.pi/2
 	psi = math.pi/2
 	
+	#set print options
 	np.set_printoptions(precision = 2, suppress = True)
-	Rx = p1_sol.rot_roll(psi)
-	Ry = p1_sol.rot_pitch(theta)
-	Rz = p1_sol.rot_yaw(phi)
+
+	# call methods from rbm to calculate matrix for each rotation
+	Rx = rbm.rot_x(psi)
+	Ry = rbm.rot_y(theta)
+	Rz = rbm.rot_z(phi)
+	
+	# matrix multiplication to determine result of Roll-Pitch-Yaw rotation
 	R1 = np.matmul(Rx, Ry)
 	R = np.matmul(R1, Rz)
 
-	print("The results of the Roll-Pitch-Yaw rotation for the given angles is: ", R)
+        #print result
+	print("The results of the Roll-Pitch-Yaw rotation for the given angles is: \n", R)
     
--- rbm.py
+++ rbm.py
@@ -4,22 +4,12 @@
 import math
 import numpy as np
 
-def rot_2d(theta):
-	"""
-	Receives an input in radians and
-	returns a 2D rotation matrix
-	R = [cos(theta) -sin(theta)
-	     sin(theta)  cos(theta)]
-	"""
-	rot = np.array([[math.cos(theta), -math.sin(theta)],
-					[math.sin(theta), math.cos(theta)]])
-	return rot
 
 # use lecture slides for the mathematical equations 
 def rot_x(psi):
 	rot = np.array([[1.0,  0.0, 0.0],
-				    [0.0, math.cos(theta), -math.sin(theta)],
-					[0.0, math.sin(theta), math.cos(theta)]])
+				    [0.0, math.cos(psi), -math.sin(psi)],
+					[0.0, math.sin(psi), math.cos(psi)]])
 	return rot
 	
 def rot_y(theta):
@@ -29,9 +19,8 @@ def rot_y(theta):
 	return rot
 	
 def rot_z(phi):
-	rot = np.array([[math.cos(theta),  -math.sin(theta), 0.0],
-					[math.sin(theta),   math.cos(theta), 0.0],
+	rot = np.array([[math.cos(phi),  -math.sin(phi), 0.0],
+					[math.sin(phi),   math.cos(phi), 0.0],
 				    [0.0, 0.0, 1.0]])
 	return rot
 	
-
